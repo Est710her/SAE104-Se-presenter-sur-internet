ef2760edf631e70ea084c5913cc0f5574a228cb0
Admin <you@example.com>
2025-11-13 10:47:41 +0100

    Initialisation du projet Symfony

---
+++ src/Controller/.gitignore.php
@@ -0,0 +1,14 @@
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BonjourController extends AbstractController
+{
+    #[Route('/bonjour', name: 'bonjour')]
+    public function index(): Response
+    {
+        return $this->render('bonjour.html.twig');
+    }
+}
