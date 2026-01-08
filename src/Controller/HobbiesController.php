a31ddcf0c2ae6984819aa901f58696689d2315af
NKENE NZAME Djust Esther <djust.esther.nkene.nzame@etu.univ-st-etienne.fr>
2026-01-08 17:21:11 +0100

    Create HobbiesController.php

---
+++ src/Controller/HobbiesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HobbiesController extends AbstractController
+{
+    #[Route('/hobbies', name: 'app_hobbies')]
+    public function index(): Response
+    {
+        return $this->render('hobbies/index.html.twig');
+    }
+}
