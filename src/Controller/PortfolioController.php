2d35ac3a004ce005f45a3019e9ce505366248b07
NKENE NZAME Djust Esther <djust.esther.nkene.nzame@etu.univ-st-etienne.fr>
2026-01-09 14:14:24 +0100

    Update PortfolioController.php

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -13,4 +13,31 @@ class PortfolioController extends AbstractController
     {
         return $this->render('portfolio/index.html.twig');
     }
+
+    #[Route('/administrer', name: 'administrer')]
+    public function administrer(): Response
+    {
+        return $this->render('portfolio/administrer.html.twig', [
+            'images' => ['/images/admin1.jpg', '/images/admin2.jpg'],
+            'description' => 'Gérer et configurer des systèmes et réseaux.',
+        ]);
+    }
+
+    #[Route('/connecter', name: 'connecter')]
+    public function connecter(): Response
+    {
+        return $this->render('portfolio/connecter.html.twig', [
+            'images' => ['/images/connect1.jpg', '/images/connect2.jpg'],
+            'description' => 'Établir et maintenir des connexions réseau fiables.',
+        ]);
+    }
+
+    #[Route('/programmer', name: 'programmer')]
+    public function programmer(): Response
+    {
+        return $this->render('portfolio/programmer.html.twig', [
+            'images' => ['/images/program1.jpg', '/images/program2.jpg'],
+            'description' => 'Développer des applications et scripts pour automatiser.',
+        ]);
+    }
 }
